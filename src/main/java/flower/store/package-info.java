3af4c9bd34680debf03211f85e9bb6747633332e
YarynaHirniak <hirniak.pn@ucu.edu.ua>
2024-10-13 22:42:58 +0300

    sttle fixed

--- src/main/java/flower/store/package-info.java
+++ src/main/java/flower/store/package-info.java
@@ -1,5 +1,5 @@
 /**
  * This package contains classes related to the flower store application,
- * including flower types, flower packs, and store functionalities.
+ * including flower types, flower packs, store functionalities.
  */
 package flower.store;
