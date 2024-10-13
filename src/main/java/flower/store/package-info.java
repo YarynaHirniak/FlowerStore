d34617080bb094891006b6fc1821a3c7af4fb1f3
YarynaHirniak <hirniak.pn@ucu.edu.ua>
2024-10-13 22:46:32 +0300

    style

--- src/main/java/flower/store/package-info.java
+++ src/main/java/flower/store/package-info.java
@@ -1,5 +1,5 @@
 /**
  * This package contains classes related to the flower store application,
- * including flower types, flower packs, store functionalities.
+ * including flower types, flower packs, store functions.
  */
 package flower.store;
